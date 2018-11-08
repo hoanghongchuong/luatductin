68c88f015477753f9892ee8cef5e3e8787277ab6
hoanghongchuong <chuonghoanghong@gmail.com>
2018-11-08 17:22:30 +0700

    a

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -16,66 +16,13 @@
   </ol>
 </section>
 <!-- Main content -->
-<section class="content">
-  
-  <div class="box box-info hidden">
-    <div class="box-header with-border">
-      <h3 class="box-title">Tin tức mới nhất</h3>
-
-      <div class="box-tools pull-right">
-        <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-        </button>
-        <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-      </div>
-    </div>
-    <!-- /.box-header -->
-    <div class="box-body">
-      <div class="table-responsive">
-        <table class="table no-margin">
-          <thead>
-          <tr>
-              <th class="text-center with_dieuhuong">Stt</th>
-              <th>Tên</th>
-              <!-- <th class="text-center with_dieuhuong">Hoạt động</th> -->
-              <th class="text-center with_dieuhuong">Sửa</th>
-              <th class="text-center with_dieuhuong">Xóa</th>
-            </tr>
-          </thead>
-          <tbody>
-          @foreach($news as $k=>$item)
-            <tr>
-              <td class="text-center with_dieuhuong">{{$k+1}}</td>
-              <td>{{$item->name}}</td>
-              <!-- <td class="text-center with_dieuhuong">
-                <div class="form-group"> 
-                  @if($item->status>0)
-                    <a href="admin/product/edit?id={{$item->id}}&hienthi={{ time() }}" class="btn btn-primary btn-xs"><i class="fa fa-eye"></i> Hiển thị</a>
-                  @else
-                    <a href="admin/product/edit?id={{$item->id}}&hienthi={{ time() }}" class="btn btn-danger btn-xs"><i class="fa fa-eye"></i> Hiển thị</a>
-                  @endif
-                </div>
-              </td> -->
-              <td class="text-center with_dieuhuong">
-                <i class="fa fa-pencil fa-fw"></i><a href="backend/news/edit?id={{$item->id}}">Edit</a>
-              </td>
-              <td class="text-center">
-                <i class="fa fa-trash-o fa-fw"></i><a onClick="if(!confirm('Xác nhận xóa')) return false;" href="backend/news/{{$item->id}}/delete">Delete</a>
-              </td>
-            </tr>
-            @endforeach
-          
-          </tbody>
-        </table>
-      </div>
-      <!-- /.table-responsive -->
-    </div>
-    <!-- /.box-body -->
-    <div class="box-footer clearfix">
-      <a href="{!! asset('admin/product') !!}" class="btn btn-sm btn-info btn-flat pull-left">Product list</a>
-      
-    </div>
-    <!-- /.box-footer -->
-  </div>
-
+<section class="content"">
+ <div class="content_img"><img src="{{ asset('public/admin_assets/images/banner.jpg') }}" class="img-responsive" alt=""></div>
+<style type="text/css" media="screen">
+  .content_img img {
+    width: 100%;
+    height: auto;
+}
+</style>
 </section><!-- /.content -->
 @endsection()
